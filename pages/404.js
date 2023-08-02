fcc1709cc097336723070e06e4650821a2841361
DO_S <rlaxogus7094@naver.com>
2023-08-02 21:19:04 +0900

    feat : 404 한글 패치

--- pages/404.js
+++ pages/404.js
@@ -14,14 +14,12 @@ export default function FourZeroFour() {
         </div>
         <div className="max-w-md">
           <p className="mb-4 text-xl font-bold leading-normal md:text-2xl">
-            Sorry we couldn't find this page.
+            요청하신 페이지를 찾을 수 없습니다
           </p>
-          <p className="mb-8">
-            But dont worry, you can find plenty of other things on our homepage.
-          </p>
+          <p className="mb-8">페이지 주소가 잘못 입력 되었거나 삭제된 페이지일 수 있습니다</p>
           <Link href="/">
             <button className="focus:shadow-outline-blue inline rounded-lg border border-transparent bg-blue-600 px-4 py-2 text-sm font-medium leading-5 text-white shadow transition-colors duration-150 hover:bg-blue-700 focus:outline-none dark:hover:bg-blue-500">
-              Back to homepage
+              메인 페이지로 이동
             </button>
           </Link>
         </div>
