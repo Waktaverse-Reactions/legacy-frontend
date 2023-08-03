9c7ed8f828928c9092e21805bdf3ed1cb06d7718
DO_S <rlaxogus7094@naver.com>
2023-08-03 12:00:57 +0900

    fix : ga

--- next.config.js
+++ next.config.js
@@ -5,11 +5,11 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 // You might need to insert additional domains in script-src if you are using external services
 const ContentSecurityPolicy = `
   default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app;
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app https://*.googletagmanager.com;
   style-src 'self' 'unsafe-inline';
-  img-src * blob: data:;
+  img-src * blob: data: https://*.google-analytics.com https://*.googletagmanager.com;
   media-src 'none';
-  connect-src *;
+  connect-src https://*.google-analytics.com https://*.analytics.google.com https://*.googletagmanager.com;
   font-src 'self';
   frame-src giscus.app
 `
