b19b631d527c88e99f5bd68ca01d6192203d2213
DO_S <rlaxogus7094@naver.com>
2023-08-05 15:50:44 +0900

    plz....

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -9,7 +9,7 @@ const siteMetadata = {
   siteRepo: 'https://github.com/DOS0313/Waktaverse-Reactions-Site',
   siteLogo: '/static/images/logo.png',
   image: '/static/images/avatar.png',
-  socialBanner: '/static/images/twitter-card.png',
+  socialBanner: '/static/images/social-banner.png',
   locale: 'en-US',
   analytics: {
     // If you want to use an analytics provider you have to add it to the
