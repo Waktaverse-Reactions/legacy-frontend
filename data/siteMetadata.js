7c92914a4dbf18fef8bc149e7847c24dbcfac230
DO_S <rlaxogus7094@naver.com>
2023-08-06 00:07:00 +0900

    design : socialBanner Change

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -9,7 +9,8 @@ const siteMetadata = {
   siteRepo: 'https://github.com/DOS0313/Waktaverse-Reactions-Site',
   siteLogo: '/static/images/logo.png',
   image: '/static/images/avatar.png',
-  socialBanner: '/static/images/social-banner.png',
+  socialBanner:
+    'https://cdn.discordapp.com/attachments/1135852744771633194/1137401140372307998/social-banner.png',
   locale: 'en-US',
   analytics: {
     // If you want to use an analytics provider you have to add it to the
